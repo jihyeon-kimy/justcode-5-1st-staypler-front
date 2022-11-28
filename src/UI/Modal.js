7de89f840a833baa02336659a37566ecc0679b01
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-11-28 15:40:12 +0900

    Refactor/modal: 확인 버튼 onClick함수 분리 - 기존에 X(닫기)버튼과 확인 버튼이 동일한 함수를 사용하도록 작성되어 있었는데, 확인 버튼에 기능 추가가 필요하여 분리

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -7,21 +7,21 @@ function BackDrop({ onClose }) {
   return <div className={css.backDrop} onClick={onClose} />;
 }
 
-function ModalOverlay({ message, onClose }) {
+function ModalOverlay({ message, onClose, onConfirm }) {
   return (
     <div className={css.modal}>
       <button type="button" className={css.closeBtn} onClick={onClose}>
-        ×
+        x
       </button>
       <p className={css.message}>{message}</p>
-      <ButtonSquare type="button" onClick={onClose}>
+      <ButtonSquare type="button" onClick={onConfirm}>
         확인
       </ButtonSquare>
     </div>
   );
 }
 
-function Modal({ message, onClose }) {
+function Modal({ message, onClose, onConfirm }) {
   return (
     <>
       {ReactDOM.createPortal(
@@ -29,7 +29,11 @@ function Modal({ message, onClose }) {
         document.getElementById('backdrop-root')
       )}
       {ReactDOM.createPortal(
-        <ModalOverlay message={message} onClose={onClose} />,
+        <ModalOverlay
+          message={message}
+          onClose={onClose}
+          onConfirm={onConfirm}
+        />,
         document.getElementById('overlay-root')
       )}
     </>
