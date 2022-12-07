4342cbe44e9895cf13b0b09d072ea375f2dc426f
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-12-07 09:33:17 +0900

    Refactor/signup: use-http, auth-api 수정사항 반영 - 유효성검사(이름, 이메일, 패스워드, 전화번호) 관련 함수를 use-input 파일로 옮겼다. login페이지에서 재사용 되고, input과 관련된 것이라 use-input파일에 함께 있는 것이 더 맞을 것 같았다. - use-http파일의 로직을 수정하고, auth관련 api를 별도 파일로 뺀 것을 signup페이지 반영했다. - 유효성이 충족되지 않을 때, submit 버튼을 비활성화 하는 것에서 충족되지 않은 부분을 모두 표시해주는 것으로 수정했다. 잘 반응해서 더 편한 느낌

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,5 +1,27 @@
 import { useState } from 'react';
 
+const REGULAR_EXPRESSION = {
+  email:
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}/,
+  password: /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})/,
+  phoneNum: /^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})/,
+};
+
+export const isName = value =>
+  value.trim() !== '' && 1 < value.length && value.length < 10;
+
+export const isEmail = value =>
+  value.trim() !== '' && REGULAR_EXPRESSION.email.test(value);
+
+export const isPassword = value =>
+  value.trim() !== '' && REGULAR_EXPRESSION.password.test(value);
+
+export const isRePassword = (Password, value) =>
+  value.trim() !== '' && Password === value;
+
+export const isPhoneNum = value =>
+  value.trim() !== '' && REGULAR_EXPRESSION.phoneNum.test(value);
+
 const useInput = validateValue => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isClicked, setIsClicked] = useState(false);
