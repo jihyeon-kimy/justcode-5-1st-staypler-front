01777026947758da703836a088e359047e00fb2f
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-12-15 00:11:48 +0900

    Refactor/Findstay: fetch로직 분리 및 로딩 상태에 따른 화면 구현 - 기존에 컴포넌트 안에 있던 fetch로직을 room-api.js파일에 분리해두었다. room-api파일에는 방 상세 정보 등 방과 직접적인 관련이 있는 fetch 로직을 모을 예정이다. - 로딩 중, 에러, 검색결과 없음, 완료 상태에 따른 화면을 나누어 구현해두었다.

--- src/pages/Findstay/Findstay.js
+++ src/pages/Findstay/Findstay.js
@@ -1,48 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import css from './Findstay.module.scss';
 import Feed from './Feed.js';
-import { BASEURL } from '../../ApiOrigin';
 import PageHeader from '../../components/PageHeader/PageHeader';
-import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import Filter from './Filter/Filter';
 import Order from './Filter/Order/Order';
+import AuthContext from '../../store/auth-context';
+import useHttp from '../../hooks/use-http';
+import { rooms } from '../../lib/room-api';
+import LoadingSpinner from '../../components/UI/LoadingSpinner/LoadingSpinner';
 
 function Findstay() {
-  const [data, setData] = useState([]);
   const location = useLocation();
+  const authCtx = useContext(AuthContext);
+
+  const {
+    sendRequest: fetchRoomsInfoHandler,
+    status,
+    data: loadedRooms,
+    error,
+  } = useHttp(rooms);
 
   let [pageBtnNum, setPageBtnNum] = useState(1);
-  let [pages, setpages] = useState();
-
-  useEffect(() => {
-    fetch(
-      `${BASEURL}/rooms${
-        location.search ? location.search + '&' : '?'
-      }page=${pageBtnNum}`,
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('login-token')}`,
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      }
-    )
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .then(fetchdata => {
-        setpages(fetchdata.maxPage);
-        setData(fetchdata.data);
-      });
-  }, [pageBtnNum, location]);
 
   const pageNumber = [];
-  for (let i = 1; i <= pages; i++) {
+  for (let i = 1; i <= loadedRooms?.maxPage; i++) {
     pageNumber.push(i);
+  }
+
+  useEffect(() => {
+    fetchRoomsInfoHandler(location.search, authCtx.token);
+  }, [authCtx.token, fetchRoomsInfoHandler, location.search]);
+
+  let loadedRoomList;
+
+  // 1. 로딩 중인 경우
+  if (status === 'pending') {
+    loadedRoomList = (
+      <div className={css.centered}>
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  // 2. fetch 에러가 발생한 경우
+  if (error) {
+    loadedRoomList = (
+      <div className={css.centered}>
+        <p>{error}</p>
+      </div>
+    );
+  }
+
+  // 3. 검색 결과가 없을 경우
+  if (
+    status === 'completed' &&
+    (!loadedRooms || loadedRooms?.data.length === 0)
+  ) {
+    loadedRoomList = (
+      <div className={css.centered}>
+        <p>검색 결과가 존재하지 않습니다.</p>
+      </div>
+    );
+  }
+
+  // 4. 로딩을 성공적으로 마친 경우
+  if (status === 'completed' && loadedRooms?.data.length > 0) {
+    loadedRoomList = loadedRooms?.data.map(feed => {
+      return (
+        <Feed
+          key={feed.id}
+          id={feed.id}
+          roomName={feed.title}
+          roomType={feed.type}
+          province={feed.province}
+          images={feed.images[0]}
+          city={feed.city}
+          maxPrice={feed.max_price}
+          minPrice={feed.min_price}
+          maxLimit={feed.max_limit}
+          minLimit={feed.min_limit}
+          isLike={feed.isLike}
+        />
+      );
+    });
   }
 
   return (
@@ -54,24 +95,9 @@ function Findstay() {
       />
       <Filter />
       <Order />
-      {data.map(feed => {
-        return (
-          <Feed
-            key={feed.id}
-            id={feed.id}
-            roomName={feed.title}
-            roomType={feed.type}
-            province={feed.province}
-            images={feed.images[0]}
-            city={feed.city}
-            maxPrice={feed.max_price}
-            minPrice={feed.min_price}
-            maxLimit={feed.max_limit}
-            minLimit={feed.min_limit}
-            isLike={feed.isLike}
-          />
-        );
-      })}
+
+      {loadedRoomList}
+
       <div className={css.pagenationWrapper}>
         {pageNumber.map(num => {
           return (
