92d49cbe15df5b588c70d50b732eda683b3d521e
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-12-19 21:04:50 +0900

    Refactor/Scroll: ScrollToTop 컴포넌트 생성 - url 경로가 변경될 경우, 스크롤을 상위로 이동하는 컴포넌트를 만들어 index.js 라우터 하단에 두었다.

---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+function ScrollToTop() {
+  const { pathname, search } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname, search]);
+}
+
+export default ScrollToTop;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import './styles/reset.scss';
 import './styles/common.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './store/auth-context';
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthContextProvider>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </AuthContextProvider>
