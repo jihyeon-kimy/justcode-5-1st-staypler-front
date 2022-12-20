c882df688344849905d2453a4f74227787cb6359
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-12-20 10:32:50 +0900

    Refactor/Findstay: RoomList컴포넌트 내로 pagination 컴포넌트 이동 - RoomList가 출력될 때에만 pagination이 필요하여, findstay에 페이지 리스트(1,2,3 ..)없이 화살표라도 기본으로 출력되는 것에서 RoomList와 함께 출력되는 것으로 수정하였다.

--- src/components/Rooms/RoomList.js
+++ src/components/Rooms/RoomList.js
@@ -1,28 +1,32 @@
+import Pagination from '../UI/Pagination/Pagination';
 import RoomItem from './RoomItem';
 import css from './RoomList.module.scss';
 
-function RoomList({ loadedRooms }) {
+function RoomList({ loadedRooms, maxPage }) {
   return (
-    <div className={css['room-list']}>
-      {loadedRooms.map(room => {
-        return (
-          <RoomItem
-            key={room.id}
-            id={room.id}
-            roomName={room.title}
-            roomType={room.type}
-            province={room.province}
-            images={room.images[0]}
-            city={room.city}
-            maxPrice={room.max_price}
-            minPrice={room.min_price}
-            maxLimit={room.max_limit}
-            minLimit={room.min_limit}
-            isLike={room.isLike}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className={css['room-list']}>
+        {loadedRooms.map(room => {
+          return (
+            <RoomItem
+              key={room.id}
+              id={room.id}
+              roomName={room.title}
+              roomType={room.type}
+              province={room.province}
+              images={room.images[0]}
+              city={room.city}
+              maxPrice={room.max_price}
+              minPrice={room.min_price}
+              maxLimit={room.max_limit}
+              minLimit={room.min_limit}
+              isLike={room.isLike}
+            />
+          );
+        })}
+      </div>
+      <Pagination maxPage={maxPage} />
+    </>
   );
 }
 export default RoomList;
