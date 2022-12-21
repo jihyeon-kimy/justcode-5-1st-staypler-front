29c84013447ed2f86c451bf96c7e873aeab8395d
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-12-21 23:33:34 +0900

    Refactor/filter: FilterItem 컴포넌트 생성

---
+++ src/components/Filter/FilterItem.js
@@ -0,0 +1,13 @@
+import css from './FilterItem.module.scss';
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+
+function FilterItem(props) {
+  return (
+    <button className={css.item} onClick={props.onClick}>
+      <p>{props.children}</p>
+      <MdOutlineKeyboardArrowDown />
+    </button>
+  );
+}
+
+export default FilterItem;
