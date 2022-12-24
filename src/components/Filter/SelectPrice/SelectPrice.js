c8e2b67fe9ab6f6edcb3150d90203c8f1c95870f
jihyeon-kimy <jihyeon.rem@gmail.com>
2022-12-24 15:30:37 +0900

    Refactor/filter: 가격 범위 필터 코드 간소화

--- src/components/Filter/SelectPrice/SelectPrice.js
+++ src/components/Filter/SelectPrice/SelectPrice.js
@@ -1,26 +1,55 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import { AiOutlineClose } from 'react-icons/ai';
 import MultiRangeSlider from './MultiRangeSlider';
+import FilterItem from '../FilterItem';
+import SelectModal from '../SelectModal/SelectModal';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-import {
-  ModalApplyBtn,
-  ModalApplyBtnWrapper,
-  ModalTitle,
-  ModalBox,
-} from '../../Filter/Filter';
+const SelectPrice = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const navigate = useNavigate();
+  const [visibleCheckBox, setVisibleCheckBox] = useState(false);
 
-const SelectPrice = ({ closeHandler }) => {
+  const toggleCheckboxHandler = () => {
+    setVisibleCheckBox(prev => !prev);
+  };
+
+  const closeCheckboxHandler = () => {
+    setVisibleCheckBox(false);
+  };
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+    let newQuery = {};
+    Array.from(event.target.price).forEach(el => (newQuery[el.id] = el.value));
+
+    queryParams.set('min_price', newQuery.min_price);
+    queryParams.set('max_price', newQuery.max_price);
+    navigate(`?${queryParams.toString()}`);
+    closeCheckboxHandler();
+  };
+
   return (
-    <ModalBox>
-      <ModalTitle>
-        가격 범위
-        <AiOutlineClose onClick={closeHandler} />
-      </ModalTitle>
+    <div>
+      <FilterItem onClick={toggleCheckboxHandler}>가격 범위</FilterItem>
 
-      <MultiRangeSlider min={0} max={100} />
-    </ModalBox>
+      {visibleCheckBox && (
+        <SelectModal
+          header="가격 범위"
+          onClose={closeCheckboxHandler}
+          type="price"
+          submitBtn="bottom"
+        >
+          <MultiRangeSlider
+            type="price"
+            onSubmit={submitHandler}
+            min={0}
+            max={100}
+          />
+        </SelectModal>
+      )}
+    </div>
   );
 };
 
